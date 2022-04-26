75f49a157fc6080ba7e716ae30b62c48581bb547
Kévy DARDOR <kevy.dardor@gmail.com>
2022-04-26 15:36:47 +0200

    Promesse

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-let users = []
+function askAge() {
+    return prompt('Quel âge avez-vous ?')
+}
 
-// fonction pour récupérer la liste des utilisateurs
-const getList = (token) => {
-    const newXhr = new XMLHttpRequest();
-    newXhr.open('GET', 'https://reqres.in/api/users?page=1');
-    newXhr.setRequestHeader('Authorization', token);
-    newXhr.addEventListener('readystatechange', () => {
-        if(newXhr.readyState === 4) {
-            if (newXhr.status === 200) {
-                users = JSON.parse(newXhr.response).data;
-                console.log(users)
-            } else {
-                alert('Erreur user')
-            }
+function success() {
+    console.log('Vous êtes majeur, vous pouvez continuer votre navigation')
+}
+
+function error() {
+    console.log('Vous êtes mineur, vous allez être redirigé vers une autre page')
+}
+
+function redirectUser() {
+    return new Promise((resolve, reject) => {
+        let age = askAge()
+
+        if (age >= 18){
+            resolve()
         }
-    });
-    newXhr.send();
+        else {
+            reject()
+        }
+    })
 }
 
-// Action au click sur le formulaire
-const sendForm = (email, password) => {
-    setTimeout(() => {
-        const token = 'ffghthfzszsz654412dsvcds';
-        getList(token);
-    }, 2000)
-}
+redirectUser().then(success, error)
