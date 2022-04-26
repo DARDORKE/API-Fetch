d30b0566faf3a5246797c6213f22557203279d24
Kévy DARDOR <kevy.dardor@gmail.com>
2022-04-26 17:05:05 +0200

    Gestion des réponses et erreurs

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function manageResponse(response) {
-    if(response.ok){
-        return response.json();
-    } else {
-        console.log(response.status)
-    }
+let urls = [
+    'https://reqres.in/api/users/2',
+    'https://reqress.in/api/users/3',
+    'https://reqres.in/api/users/23'
+]
+
+function fetchUrl(url) {
+    fetch(url)
+        .then((response) => {
+            if(response.ok){
+                return response.json();
+            } else {
+                console.log('Une erreur est survenue, code erreur ' + response.status)
+                return {}
+            }
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => console.error(error)); //Traitement de l'erreur dans l'appel
 }
 
-function displayData(data) {
-    console.log(data)
-}
-
-fetch('https://reqres.in/api/users/2')
-    .then(manageResponse)
-    .then(displayData)
+urls.forEach(url => fetchUrl(url))
