627de247f632e1481fba34e17017fb4c3a7f84dd
Kévy DARDOR <kevy.dardor@gmail.com>
2022-04-26 16:54:36 +0200

    Fetch API

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
-function askUsername() {
-    return prompt('Quel est votre nom d\'utilisateur ?')
+function manageResponse(response) {
+    if(response.ok){
+        return response.json();
+    } else {
+        console.log(response.status)
+    }
 }
 
-function askMathOperation() {
-    return prompt('Combien font 2 + 2')
+function displayData(data) {
+    console.log(data)
 }
 
-function success() {
-    console.log('Vous pouvez accéder à l\'application')
-}
-
-function error() {
-    console.log('Restez où vous êtes')
-}
-
-function checkUsername() {
-    return new Promise((resolve, reject) => {
-        let username = askUsername()
-
-        if ('admin' === username) {
-            resolve()
-        } else {
-            reject()
-        }
-    })
-}
-
-function checkIfIsBot() {
-    return new Promise((resolve, reject) => {
-        let result = askMathOperation()
-        if (4 === parseInt(result)) {
-            resolve()
-        } else {
-            reject()
-        }
-    })
-}
-
-Promise.all([checkUsername(), checkIfIsBot()]).then(success, error)
+fetch('https://reqres.in/api/users/2')
+    .then(manageResponse)
+    .then(displayData)
